4aa1728adcb42f72b04213b02f3b4051bf6b3766
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-10-09 23:31:53 -0700

    E3-1.DiazUriasJorgeDavid

---
+++ E3-1.DiazUriasJorgeDavid/E3-1.DiazUriasJorgeDavid/Metodos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace E3_1.DiazUriasJorgeDavid
+{
+    class Metodos
+    {
+        public void Captura(int Cantidad)
+        {
+            //se crean 2 array list, nombre de la materia y califiacion de alumno
+            ArrayList Materia = new ArrayList();
+            ArrayList Calificacion = new ArrayList();
+            //ciclos for anidados para captura de datos de materia y calificacion
+            for (int i = 0; i < Cantidad; i++)
+            {
+                Console.WriteLine("Ingrese el nombre de la materia {0}", i + 1);
+                Materia.Add(Console.ReadLine()); //se añade el nombre de la materia
+                for (int j = 0; j < Cantidad; j++)
+                {
+                    Console.WriteLine("ingrese la calificacion del alumno {0}", j + 1);
+                    Calificacion.Add(Console.ReadLine()); //se añade la califiacion del alumno
+                }
+            }
+            //impresion del arraylist 
+            foreach (var item in Materia)
+            {
+                Console.WriteLine(item);
+                foreach (var item2 in Calificacion)
+                {
+                      Console.WriteLine(item2);        
+                }
+            }
+        }
+    }
+}
---
+++ E3-1.DiazUriasJorgeDavid/E3-1.DiazUriasJorgeDavid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace E3_1.DiazUriasJorgeDavid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cantidad;//declaracion de variable
+            Console.WriteLine("Ingrese el numero de clases y alumnos");
+            Cantidad = int.Parse(Console.ReadLine());
+            Metodos Clase = new Metodos();
+            //se llama al metodo captura y se envia como parametro la cantidad
+            Clase.Captura(Cantidad);
+            Console.ReadKey();
+        }
+    }
+}
