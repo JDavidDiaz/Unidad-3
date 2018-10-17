bc40363cc5e7cb9074b08258001c9e02d2adb5f1
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-10-16 17:40:15 -0700

    E3-2.DiazUriasJorgeDavid

---
+++ E3-2.DiazUriasJorgeDavid/E3-2.DiazUriasJorgeDavid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace E3_2.DiazUriasJorgeDavid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Cola = new Queue();
+            //Enqueue: Agrega un elemento al final de la cola, este elemento puede ser de cualquier tipo
+            Cola.Enqueue("Hi");
+            Cola.Enqueue(4);
+            Cola.Enqueue(true);
+            foreach (var item in Cola)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();    
+            //Dequeue: Elimina el primer elemento de la cola
+            Cola.Dequeue();
+            foreach (var item in Cola)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            //TrimToSize: Restringe la capacidad de la cola con el numero de elementos actuales de la cola 
+            //En el caso de las colas no es posible visualizar la utilizacion de TrimToSize debido a que las colas no cuentan con una propiedad Capacity
+            Cola.TrimToSize();
+            Console.ReadKey();
+        }
+    }
+}
