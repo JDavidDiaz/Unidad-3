867a3f5d8648e3165cb02e410b25918f1a6b43ce
JDavidDiaz <43283432+JDavidDiaz@users.noreply.github.com>
2018-10-21 17:39:34 -0700

    E3-3.DiazUriasJorgeDavid

---
+++ E3.3-DiazUriasJorgeDavid/BlackJack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.OutputEncoding = Encoding.Unicode; //Permite visualizar caracteres especiales (unicode)
+            Console.WriteLine("Bienvenido al Juego '♥♦♣♠ BlackJack ♥♦♣♠'");
+            Console.WriteLine("Nota: En Caso de que salga un 'As' la suma se tomara en cuenta hasta el\nfinal para tomar su valor segun la situacion que convenga");
+            Console.ReadLine();
+            Metodos Baraja = new Metodos(); //Creacion del objeto
+            Baraja.Juego(); //Se llama al metodo que ejecutara el juego
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E3.3-DiazUriasJorgeDavid/BlackJack/ValorCarta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class ValorCarta
+    {
+        //Se crean propiedades donde get se utiliza para obtener el valor de la propiedad y set se utiliza para asignar el valor a la propiedad.
+        public object Numero { get; set; }
+        public string Simbolo { get; set; }
+    }
+}
